5c50de18882a5888a0fa1fcc3a4c18b851f156de
yama3ki <yama3ki.tada4@gmail.com>
2018-10-10 09:46:32 +0900

    initial commit

---
+++ app/src/main/java/com/example/ore/rss_reader001/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ore.rss_reader001
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
